37a832488488a73bf946b53e82b0972ae2571949
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-05-18 10:22:34 +0200

    Add init() in app.py

--- universs/app.py
+++ universs/app.py
@@ -2,9 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Flask
+from flask import g
+
 from base import init as dbinit
 
 app = Flask(__name__)
+
+@app.before_request
+def init():
+
+    g.db = db = dbinit()
+    g.feeds = list(db.feeds.find())
+    g.tags = list(db.tags.find())
+    g.agents = list(db.agents.find())
+    g.filters = list(db.filters.find())
 
 if __name__ == '__main__':
 
