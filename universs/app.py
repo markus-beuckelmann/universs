cd5ac9ab89783377332e8f7e42ebd905c219b296
Markus Beuckelmann <email@markus-beuckelmann.de>
2017-05-21 15:25:54 +0200

    Add tasks route to app.py

--- universs/app.py
+++ universs/app.py
@@ -2,11 +2,21 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Flask
-from flask import g, render_template
+from flask import g, render_template, jsonify
+
+from celery import Celery
 
 from base import init as dbinit
 
 app = Flask(__name__)
+
+# Celery configuration
+# app.config['CELERY_BROKER_URL'] = 'mongodb://localhost:27017/celery'
+# app.config['CELERY_RESULT_BACKEND'] = 'mongodb'
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+
+celery = Celery(app.import_name, backend = app.config['CELERY_RESULT_BACKEND'], broker = app.config['CELERY_BROKER_URL'])
 
 @app.before_request
 def init():
@@ -16,6 +26,26 @@ def init():
     g.tags = list(db.tags.find())
     g.agents = list(db.agents.find())
     g.filters = list(db.filters.find())
+
+@app.route('/tasks/<string:action>')
+@app.route('/tasks/<string:action>/title/<string:title>')
+@app.route('/tasks/<string:action>/id/<string:identifier>')
+def tasks(action, title = None, identifier = None):
+
+    from tasks import update, download, process, update_feed_metadata, update_tag_metadata
+
+    if action in ('update', 'download', 'process', 'update_feed_metadata', 'update_tag_metadata'):
+        if action in locals():
+            f = locals()[action]
+            if title:
+                f.delay(title = title)
+            elif identifier:
+                f.delay(identifier = identifier)
+            else:
+                f.delay()
+        return jsonify({'message' : 'Ok', 'status' : 202, 'mimetype' : 'application/json'})
+
+    jsonify({'message' : 'Not implemented', 'status' : 501, 'mimetype' : 'application/json'})
 
 @app.route('/analytics')
 def analytics():
